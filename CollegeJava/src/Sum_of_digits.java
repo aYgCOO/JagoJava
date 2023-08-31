12c1968e0863add8b872d8f952a5327670919daa
Ayanavbha Chatterjee <117476380+aYgCOO@users.noreply.github.com>
2023-08-31 22:30:47 +0530

    new
    new code

---
+++ CollegeJava/src/Sum_of_digits.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Sum_of_digits {
+    public static void main(String arg[]){
+        int i=0,sum=0,n,r;
+        System.out.println("enter  numbers: ");
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        for(i=0;n>0;i++){
+            r=n%10;
+            sum=sum+r;
+            n=n/10;
+
+        }
+        System.out.println(sum);
+    }
+}
