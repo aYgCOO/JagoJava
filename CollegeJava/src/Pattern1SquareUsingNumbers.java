9363694a05ce5f74661fce5f437ebc7ecd793ac7
Ayanavbha Chatterjee <117476380+aYgCOO@users.noreply.github.com>
2023-09-10 19:05:45 +0530

    Saved
    Update code

--- CollegeJava/src/Pattern1SquareUsingNumbers.java
+++ CollegeJava/src/Pattern1SquareUsingNumbers.java
@@ -1,21 +1,3 @@
-//import java.util.*;
-//public class Parttern1SqureUsingNumbers {
-//    public static void main(String arg[]){
-//        int i, j, number;
-//        System.out.println("Enter a number sequence: ");
-//        Scanner sc=new Scanner(System.in);
-//        number=sc.nextInt();
-//        for (i=1; i<=number; i++){
-//            for (j=1; j<=number; j++){
-//                System.out.println(j);
-//            }
-//            System.out.println();
-//
-//        }
-//
-//    }
-//}
-
 import java.util.*;
 
 public class Pattern1SquareUsingNumbers {
